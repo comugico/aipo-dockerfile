8edc99bf9fbb5319f91749b7456161f505ac924e
Shuma Yoshioka <s64.stdio@gmail.com>
2014-08-22 08:54:15 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pushd /opt
+
+tar -xvzf "downloaded.tar.gz"
+pushd "aipo7020aja_linux"
+tar -xvzf "aipo7020.tar.gz"
+mv "./aipo" /opt
+popd
+
+rm "downloaded.tar.gz"
+rmdir "aipo7020aja_linux"
+
+tree .
+
+pushd "./aipo/bin"
+sh installer.sh
+popd
+
+popd
